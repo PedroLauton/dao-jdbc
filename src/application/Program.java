fdf3ea4feacec7cb787d1c903233a55f8e00e52f
PedroLauton <lautonpedro@gmail.com>
2024-10-21 12:41:56 -0300

    Implementando a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dp = new Department(1, "Books"); 
+		Seller sl = new Seller(1, "Pedro", "pedro@gmail.com", LocalDate.now(), 2000.99, dp); 
 		System.out.println(dp);
+		System.out.println(sl);
 	}
 
 }
