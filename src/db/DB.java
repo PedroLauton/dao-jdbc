562276e70a8e27d8ea33e39bb633127b9edcee17
PedroLauton <lautonpedro@gmail.com>
2024-10-21 11:58:28 -0300

    Implementação da classe Department

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,5 +1,69 @@
 package db;
 
-public class DB {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() {
+		if(conn == null) {
+			try {
+				Properties props = loadPropreties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException("Erro em realziar a conexão com o banco!");
+			}
+			
+		}
+		return conn;
+	}
+	
+	public static void closeConnection(Connection conn) {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException("Erro no fechamento do banco."); 
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException("Erro no fechamento do resultSet"); 
+			}
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException("Erro no fechamento do resultSet"); 
+			}
+		}
+	}
+	
+	private static Properties loadPropreties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")){
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch(IOException e) {
+			throw new DbException("Erro na leitura das informações do banco de dados.");
+		}
+	}
 }
